1cfcd4ee6112a53f4f2b8d8409143a7063fd2e5f
JB <jeanbastien.grill@gmail.com>
2017-01-09 16:15:08 +0100

    Slice refactor

--- DeepPython/Slice.py
+++ DeepPython/Slice.py
@@ -1,71 +1,95 @@
 from copy import deepcopy
 import random
+import Params
 
 
 class Slice:
-    def __init__(self, py_datas, group, feed_dict={}):
-        
-        self.group = group
-        self.parameters = feed_dict
-        
-        if group == 'Lpack':
-            self.slices = {'train': [], 'test': []}
-            current_journee = -1
-            current_nb_slice = -1
-            nb_matchs = 0
-            current_slice_test = {'left': 0, 'right': 0}
-            current_slice_train = {'left': 0, 'right': 0}
-            for row in py_datas:
-                if current_journee != row["journee"] and nb_matchs > 3800:
-                    current_journee = row["journee"]
-                    current_slice_test['right'] = nb_matchs + 1
-                    current_slice_test['name'] = 'test_' + str(current_nb_slice)
-                    current_slice_train['right'] = nb_matchs + 1
-                    current_slice_train['name'] = 'train_' + str(current_nb_slice+1)
-                    current_nb_slice += 1
-                    if current_nb_slice > 0:
-                        self.slices['test'].append(deepcopy(current_slice_test))
-                    self.slices['train'].append(deepcopy(current_slice_train))
-                    current_slice_test['left'] = nb_matchs + 1
-                nb_matchs += 1
-            self.nb_slices = min(len(self.slices['test']), len(self.slices['train']))
-            self.check_slices(group)
+    def __init__(self, py_datas, group, feed_dict=None):
 
-        elif group == 'Shuffle':
-            self.nb_slices = 1
-            self.internal_seed = random.getrandbits(64)
+        if feed_dict is None:
+            feed_dict = {}
 
+        self.switcher = {
+            'Lpack': {'init': self.__init_lpack,
+                      'get_slice': self.__get_slice_lpack},
+            'Shuffle': {'init': self.__init_shuffle,
+                        'get_slice': self.__get_slice_shuffle}
+        }
+
+        if group not in self.switcher:
+            raise('Unkown type ' + group)
+
+        self.__group = group
+        self.__parameters = feed_dict
+        self.__slices = {}
+
+        self.nb_slices = 0
+
+        self.switcher[self.__group]['init'](py_datas)
+
+        if Params.CHECK_SLICE_OVERLAP:
+            self.__check_slices()
+
+    def shuffle_slice(self):
+        self.__slices['internal_seed'] = random.getrandbits(64)
+
+    def get_slice(self, feed_dict):
+        return lambda l: self.switcher[self.__group]['get_slice'](l, feed_dict)
+
+    def __init_lpack(self, py_datas):
+        self.__slices = {'train': [], 'test': []}
+        current_journee = -1
+        current_nb_slice = -1
+        nb_matchs = 0
+        current_slice_test = {'left': 0, 'right': 0}
+        current_slice_train = {'left': 0, 'right': 0}
+        for row in py_datas:
+            if current_journee != row["journee"] and nb_matchs > 3800:
+                current_journee = row["journee"]
+                current_slice_test['right'] = nb_matchs + 1
+                current_slice_test['name'] = 'test_' + str(current_nb_slice)
+                current_slice_train['right'] = nb_matchs + 1
+                current_slice_train['name'] = 'train_' + str(current_nb_slice + 1)
+                current_nb_slice += 1
+                if current_nb_slice > 0:
+                    self.__slices['test'].append(deepcopy(current_slice_test))
+                self.__slices['train'].append(deepcopy(current_slice_train))
+                current_slice_test['left'] = nb_matchs + 1
+            nb_matchs += 1
+        self.nb_slices = min(len(self.__slices['test']), len(self.__slices['train']))
+        self.__quick_check_slices(self.__group)
+
+    def __get_slice_lpack(self, l, feed_dict):
+        left = self.__slices[feed_dict['label']][feed_dict['index']]['left']
+        right = self.__slices[feed_dict['label']][feed_dict['index']]['right']
+        return l[left:right]
+
+    def __init_shuffle(self, _):
+        self.nb_slices = 1
+        self.__slices['internal_seed'] = random.getrandbits(64)
+
+    def __get_slice_shuffle(self, l, feed_dict):
+        n = len(l)
+        random.seed(a=self.__slices['internal_seed'])
+        if feed_dict['label'] == 'train':
+            return random.sample(l, n)[:int(n * self.__parameters['p_train'])]
         else:
-            print('Unkown type ' + type)
+            return random.sample(l, n)[int(n * self.__parameters['p_train']):]
 
-    def check_slices(self, group):
+    def __check_slices(self):
+        pass
+
+    def __quick_check_slices(self, group):
         if group == 'Lpack':
             for i in range(self.nb_slices):
-                if self.slices['train'][i]['right'] <= self.slices['test'][i]['left'] or self.slices['train'][i]['left'] >= self.slices['test'][i]['right']:
-                    if self.slices['train'][i]['name'] != 'train_'+str(i) or self.slices['test'][i]['name'] != 'test_'+str(i):
+                if self.__slices['train'][i]['right'] <= self.__slices['test'][i]['left'] or self.__slices['train'][i]['left'] >= self.__slices['test'][i]['right']:
+                    if self.__slices['train'][i]['name'] != 'train_'+str(i) or self.__slices['test'][i]['name'] != 'test_'+str(i):
                         print('slice error')
                 else:
                     print('overlapping train and test')
 
-    def shuffle_list(self, l, p, train):
-        n = len(l)
-        random.seed(a=self.internal_seed)
-        if train:
-            return random.sample(l, n)[:int(n*p)]
-        else:
-            return random.sample(l, n)[int(n*p):]
 
-    def shuffle_slice(self):
-        self.internal_seed = random.getrandbits(64)
 
-    def get_slice(self, feed_dict):
-        if self.group in ['Lpack']:
-            return lambda l: l[self.slices[feed_dict['label']][feed_dict['index']]['left']:self.slices[feed_dict['label']][feed_dict['index']]['right']]
-        elif self.group == 'Shuffle':
-            return lambda l: self.shuffle_list(l, self.parameters['p_train'], feed_dict['label'] == 'train')
-        else:
-            print('warning Slices.get_slices')
-            
                 
             
 
