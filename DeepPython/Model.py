c819e6d00f2a3304bd57cfae7ee942a20e1162bc
Pytq <jeanbastien.grill@gmail.com>
2017-01-23 21:21:46 +0100

    0.1 is done !

--- DeepPython/Model.py
+++ DeepPython/Model.py
@@ -3,8 +3,12 @@ from DeepPython import ToolBox
 
 
 class Model:
-    def __init__(self, data_dict=None, targets=None, customizator=None, name='untitle'):
+    def __init__(self, data_dict=None, targets=None, customizator=None, name='untitle', session=None):
 
+        if session is None:
+            raise Exception('A session must be fed')
+        else:
+            self.session = session
         if targets is None:
             targets = ['res']
         if customizator is None:
@@ -12,27 +16,26 @@ class Model:
         self.customizator = customizator
 
         self.data_dict = data_dict
-
         self.name = name
         self.costs = {}
         self.train_step = {}
-        self.session = None
-        self.dictparam = {}
-        self.init_all = None
+        self.dictparam = None
 
-        self.param = {}
+        # Define trainable_parameters
+        self.trainable_params = {}
         self.define_parameters()
 
-        self.metaparam = {}
+        # Define given_parameters
+        self.given_params = {}
         self.ph_metaparam = {}
         for key in self.meta_params():
+            self.ph_metaparam[key] = tf.placeholder(tf.float32)
             if key in self.linear_meta_params():
-                self.ph_metaparam[key] = tf.placeholder(tf.float32)
-                self.metaparam[key] = self.ph_metaparam[key]
+                self.given_params[key] = self.ph_metaparam[key]
             else:
-                self.ph_metaparam[key] = tf.placeholder(tf.float32)
-                self.metaparam[key] = tf.exp(self.ph_metaparam[key])
+                self.given_params[key] = tf.exp(self.ph_metaparam[key])
 
+        # Define slice node
         self.current_slice = {}
         self.ph_current_slice = {}
         self.tf_assign_slice = []
@@ -43,56 +46,58 @@ class Model:
             assign_slice = tf.assign(self.current_slice[key], self.ph_current_slice[key], validate_shape=False)
             self.tf_assign_slice.append(assign_slice)
 
+        # Define reset_trainable_parameters node
+        params = [self.trainable_params[key] for key in self.trainable_params]
+        self.reset_op = None
+        if params != []:
+            self.reset_op = tf.variables_initializer(params, name='{}.reset_trainable_parameters'.format(self.name))
 
-        params = [self.param[key] for key in self.param]
-        self.reset_op = tf.variables_initializer(params, name='init')
-
+        # Define prediction node
         self.prediction = {}
         for key in targets:
             self.prediction[key] = self.get_prediction(self.current_slice, key)
 
+        # Define regularizer
         self.regulizer = self.get_regularizer()
 
     def finish_init(self):
-        # Create the session
-        self.init_all = tf.global_variables_initializer()
         print('Model {} created. {} nodes in tf.Graph'.format(self.name, ToolBox.nb_tf_op()))
-        self.new_session()
+        if self.reset_op is not None:
+            self.reset_trainable_parameters()
 
     def add_cost(self, cost, trainable=False):
         if cost.name in self.costs:
             print('Cost with name {} is already defined'.format(cost.name))
         else:
-            self.costs[cost.name] = cost.get_cost(self)
-            if trainable and self.is_trainable():
-                self.train_step[cost.name] = tf.train.AdamOptimizer(0.01).minimize(self.costs[cost.name])
+            self.costs[cost.name] = {}
+            self.costs[cost.name]['cost'] = cost.get_cost(self)
+            if trainable and not self.is_trainable():
+                raise Exception('Model {} is not trainable yet is given cost {} which is trainable'.format(self.name, cost.name))
+            elif trainable and self.is_trainable():
+                self.costs[cost.name]['train_step'] = tf.train.AdamOptimizer(0.01).minimize(self.costs[cost.name])
+
 
     def set_params(self, param):
-        if self.name != 'regr':
-            self.dictparam = {self.ph_metaparam[key]: value for key, value in param.items() if key in self.ph_metaparam}
+        self.dictparam = {self.ph_metaparam[key]: value for key, value in param.items() if key in self.ph_metaparam}
 
-    def reset(self):
-        self.session.run(self.reset_op)
-
-    def new_session(self):
-        if self.session is not None:
-            self.session.close()
-        self.session = tf.Session()
-        self.session.run(self.init_all)
+    def reset_trainable_parameters(self):
+        if self.reset_op is not None:
+            self.session.run(self.reset_op)
+        else:
+            raise Exception('Cannot reset trainable parameters of model {} which has no such parameters'.format(self.name))
 
     def train(self, cost, iterations):
         for _ in range(iterations):
-            self.run(self.train_step[cost])
+            self.run(self.costs[cost]['train_step'])
 
     def get_cost(self, cost):
-        return self.run(self.costs[cost])
+        return self.run(self.costs[cost]['cost'])
 
-    def run(self, x):
-        return self.session.run(x, feed_dict=self.dictparam)
-
-    def close(self):
-        print('Session closed with {} nodes in tf.Graph'.format(ToolBox.nb_tf_op()))
-        self.session.close()
+    def run(self, op_to_run):   # op is a tensorflow node and stands for operator.
+        if self.dictparam is not None:
+            return self.session.run(op_to_run, feed_dict=self.dictparam)
+        else:
+            raise Exception('Given parameters must be initialized for model: {}'.format(self.name))
 
     def meta_params(self):
         return []
@@ -104,7 +109,7 @@ class Model:
         return []
 
     def define_parameters(self):
-        return []
+        pass
 
     def get_prediction(self, s, target):
         raise NotImplementedError
@@ -113,4 +118,4 @@ class Model:
         return tf.constant(0.)
 
     def is_trainable(self):
-        return True
+        return self.trainable_params != {}
